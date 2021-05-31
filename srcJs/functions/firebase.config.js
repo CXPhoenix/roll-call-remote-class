4d88e9ea7f9f31170de5d48ea8cfd5dd049088f7
CXPhoenix <45819421+CXPhoenix@users.noreply.github.com>
2021-05-31 19:45:10 +0800

    update: .gitignore

---
+++ srcJs/functions/firebase.config.js
@@ -0,0 +1,10 @@
+import { firebaseConfig } from './firebaseConfig.js'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+
+export const app = firebase.initializeApp(firebaseConfig)
+export const apiKey = firebaseConfig.apiKey
+export const auth = firebase.auth
+export const db = firebase.firestore()
