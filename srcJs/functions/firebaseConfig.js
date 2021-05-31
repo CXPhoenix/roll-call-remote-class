c3b37541e2866d194c27494676444a9a5a2c9257
CXPhoenix <45819421+CXPhoenix@users.noreply.github.com>
2021-05-31 19:47:04 +0800

    Delete firebaseConfig.js

--- srcJs/functions/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyAyqMZQPC7G6XbukqxrHZoERhDj_r4EDSQ",
-    authDomain: "rollcall-c365a.firebaseapp.com",
-    projectId: "rollcall-c365a",
-    storageBucket: "rollcall-c365a.appspot.com",
-    messagingSenderId: "727999114140",
-    appId: "1:727999114140:web:0a9a7914247ea548def659",
-    measurementId: "G-MFJCXN5MKX"
-};
