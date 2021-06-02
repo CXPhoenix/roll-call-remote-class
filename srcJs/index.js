5b80407552d490018e4adb19f9897afcd23e7f78
CXPhoenix <45819421+CXPhoenix@users.noreply.github.com>
2021-06-02 15:24:40 +0800

    update: index.js and roll-call-link.html

--- srcJs/index.js
+++ srcJs/index.js
@@ -7,27 +7,35 @@ import {
 // init page element
 const loginBtn = document.querySelector('#login')
 const authSelect = document.querySelector('#authSelect')
+const loginBtnArea = document.querySelector('#isLogin')
 
 // google login init
 const google = new auth.GoogleAuthProvider
-google.addScope('https://www.googleapis.com/auth/classroom.courses.readonly')
-google.addScope('https://www.googleapis.com/auth/classroom.rosters.readonly')
-google.addScope('https://www.googleapis.com/auth/classroom.profile.emails')
+// google.addScope('https://www.googleapis.com/auth/classroom.courses.readonly')
+// google.addScope('https://www.googleapis.com/auth/classroom.rosters.readonly')
+// google.addScope('https://www.googleapis.com/auth/classroom.profile.emails')
 
 // on load event
 window.addEventListener('load', () => {
     console.log('load')
-    checkHash()
-    window.sessionStorage.clear()
+    if (checkSearch()) {
+        authSelect.style.display = 'none'
+        setTips(loginBtnArea, '請使用已加入課程的帳號進行登入點名！')
+    } else {
+        google.addScope('https://www.googleapis.com/auth/classroom.courses.readonly')
+        google.addScope('https://www.googleapis.com/auth/classroom.rosters.readonly')
+        google.addScope('https://www.googleapis.com/auth/classroom.profile.emails')
+    }
+    // window.sessionStorage.clear()
 })
 
 // log in button
 // check the url hash for deciding the action
 loginBtn.addEventListener('click', () => {
-    if (checkHash()) {
-        storageHash(window.location.hash)
+    if (checkSearch()) {
+        storageSearch(window.location.Search)
         GoogleLogin('./roll-call.html')
-    }else if (authSelect.value === '請問你的身份？') {
+    } else if (authSelect.value === '請問你的身份？') {
         alert('請一定要選擇一個身份喔！')
     } else {
         GoogleLogin(authSelect.value)
@@ -48,20 +56,31 @@ function GoogleLogin(page) {
     })
 }
 
-function checkHash() {
-    if (window.location.hash) {
-        authSelect.style.display = 'none'
+function checkSearch() {
+    if (window.location.search) {
         return true
     }
     return false
 }
 
-function storageHash(urlHash) {
-    urlHash = urlHash.split('#')[1].split('/')
-    const rollCallCourse = urlHash[0]
-    const rollCallTimestamp = urlHash[1]
-    const state = urlHash[2]
+function storageSearch(urlSearch) {
+    urlSearch = urlSearch.split('?')[1].split('-')
+    const rollCallCourse = urlSearch[0]
+    const rollCallTimestamp = urlSearch[1]
+    const state = urlSearch[2]
     window.sessionStorage.setItem('rollCallCourse', rollCallCourse)
     window.sessionStorage.setItem('rollCallTimestamp', rollCallTimestamp)
     window.sessionStorage.setItem('state', state)
+}
+
+function setTips(parentNode, tipsText) {
+    const tipArea = document.createElement('div')
+    tipArea.className = 'text-center mt-1 mb-3'
+    
+    const tipSpan = document.createElement('span')
+    tipSpan.className = 'border border-danger border-3 m-2 p-2 fs-4'
+    tipSpan.innerText = tipsText
+    
+    tipArea.appendChild(tipSpan)
+    parentNode.appendChild(tipArea)
 }
